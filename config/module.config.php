373c155893d418279065f7c649ff6bc6b1a5280b
Tim Horie <tak@thorie.com>
2015-04-12 18:30:58 -0700

    Updated sources for ZF2 modularization

---
+++ config/module.config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+return array(
+    'service_manager' => array(
+        'aliases'   => array(
+            'Bitcoin\Client' => 'Bitcoin\BitcoinClient',
+        ),
+        'factories' => array(
+            'Bitcoin\Client' => 'Bitcoin\Factory\ClientFactory',
+        )
+    ),
+    'view_manager' => array(
+        // Enable this in your application configuration in order to get full
+        // exception stack traces in your API-Problem responses.
+        'display_exceptions' => false,
+    ),
+);
